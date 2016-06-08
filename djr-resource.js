7b6adc9107ca49194c4e38c5d5516a7abd8ea7fb
Arturo B <arturoblack0@gmail.com>
2016-06-08 12:05:39 -0500

    read and copy ngResource Code

--- djr-resource.js
+++ djr-resource.js
@@ -1,148 +1,241 @@
-'use strict';
-/* jshint -W097 */
-/* global angular */
-
-//Portions of this file:
-//Copyright (c) 2010-2012 Google, Inc. http://angularjs.org
-//Those portions modified, used, or copied under permissions granted by the MIT license. See:
-// https://raw.github.com/angular/angular.js/9480136d9f062ec4b8df0a35914b48c0d61e0002/LICENSE
-
 /**
- * @ngdoc overview
- * @name djangoRESTResources
- * @description
- */
+ * @license DjrResoruce - Django REST Resource for AngularJS v1.5.6
+ * (c) 2016-2016
+ * License: MIT
+*/
+(function(window, angular) {'use strict';
+  var $resourceMinErr = angular.$$minErr('$djrResource');
 
-/**
- * @ngdoc object
- * @name djangoRESTResources.djResource
- * @requires $http
- *
- * @description
- * A factory for generating classes that interact with a Django REST Framework backend.
- *
- * Identical in operation to AngularJS' ngResource module's $resource object except for the following:
- *  - If an isArray=True request receives a JSON _object_ containing a `count` field (instead of a JS array), assume
- *  that the REST endpoint has `paginate_by` set. The results are then streamed a page at a time into the promise object
- *  and any success callbacks are deferred until the last page returns successfully.
- *  - URLs are assumed to have the trailing slashes, as is the Django way of doing things.
- *
- * # Installation
- * Include `angular-django-rest-resource.js`
- *
- * Load the module:
- *
- *        angular.module('app', ['djangoRESTResources']);
- *
- * now you inject djResource into any of your Angular things.
- *
- * @param {string} url A parametrized URL template with parameters prefixed by `:` as in
- *   `/user/:username`. If you are using a URL with a port number (e.g.
- *   `http://example.com:8080/api`), it will be respected.
- *
- * @param {Object=} paramDefaults Default values for `url` parameters. These can be overridden in
- *   `actions` methods. If any of the parameter value is a function, it will be executed every time
- *   when a param value needs to be obtained for a request (unless the param was overridden).
- *
- *   Each key value in the parameter object is first bound to url template if present and then any
- *   excess keys are appended to the url search query after the `?`.
- *
- *   Given a template `/path/:verb` and parameter `{verb:'greet', salutation:'Hello'}` results in
- *   URL `/path/greet?salutation=Hello`.
- *
- *   If the parameter value is prefixed with `@` then the value of that parameter is extracted from
- *   the data object (useful for non-GET operations).
- *
- * @param {Object.<Object>=} actions Hash with declaration of custom action that should extend the
- *   default set of resource actions. The declaration should be created in the format of $http.config
- *
- *       {action1: {method:?, params:?, isArray:?, headers:?, ...},
- *        action2: {method:?, params:?, isArray:?, headers:?, ...},
- *        ...}
- *
- *   Where:
- *
- *   - **`action`** – {string} – The name of action. This name becomes the name of the method on your
- *     resource object.
- *   - **`method`** – {string} – HTTP request method. Valid methods are: `GET`, `POST`, `PUT`, `DELETE`,
- *     and `JSONP`.
- *   - **`params`** – {Object=} – Optional set of pre-bound parameters for this action. If any of the
- *     parameter value is a function, it will be executed every time when a param value needs to be
- *     obtained for a request (unless the param was overridden).
- *   - **`url`** – {string} – action specific `url` override. The url templating is supported just like
- *     for the resource-level urls.
- *   - **`isArray`** – {boolean=} – If true then the returned object for this action is an array, see
- *     `returns` section.
- *   - **`transformRequest`** – `{function(data, headersGetter)|Array.<function(data, headersGetter)>}` –
- *     transform function or an array of such functions. The transform function takes the http
- *     request body and headers and returns its transformed (typically serialized) version.
- *   - **`transformResponse`** – `{function(data, headersGetter)|Array.<function(data, headersGetter)>}` –
- *     transform function or an array of such functions. The transform function takes the http
- *     response body and headers and returns its transformed (typically deserialized) version.
- *   - **`cache`** – `{boolean|Cache}` – If true, a default $http cache will be used to cache the
- *     GET request, otherwise if a cache instance built with
- *     {@link http://docs.angularjs.org/api/ng.$cacheFactory $cacheFactory}, this cache will be used for
- *     caching.
- *   - **`timeout`** – `{number}` – timeout in milliseconds.
- *   - **`withCredentials`** - `{boolean}` - whether to to set the `withCredentials` flag on the
- *     XHR object. See {@link https://developer.mozilla.org/en/http_access_control#section_5
- *     requests with credentials} for more information.
- *   - **`responseType`** - `{string}` - see
- *   {@link https://developer.mozilla.org/en-US/docs/DOM/XMLHttpRequest#responseType requestType}.
- *
- * @returns {Object} A resource "class" object with methods for the default set of resource actions
- *   optionally extended with custom `actions`. The default set contains these actions:
- *
- *       { 'get':    {method:'GET'},
- *         'save':   {method:'POST', method_if_field_has_value:['id', 'PUT']},
- *         'update': {method:'PUT'},
- *         'query':  {method:'GET', isArray:true},
- *         'remove': {method:'DELETE'},
- *         'delete': {method:'DELETE'} };
- *
- *   Calling these methods invoke an {@link http://docs.angularjs.org/api/ng.$http $http} with the specified http
- *   method, destination and parameters. When the data is returned from the server then the object is an
- *   instance of the resource class. The actions `save`, `remove` and `delete` are available on it
- *   as  methods with the `$` prefix. This allows you to easily perform CRUD operations (create,
- *   read, update, delete) on server-side data like this:
- *   <pre>
-        var User = djResource('/user/:userId', {userId:'@id'});
-        var user = User.get({userId:123}, function() {
-          user.abc = true;
-          user.$save();
-        });
-     </pre>
- *
- *   Invoking a djResource object method immediately returns an empty reference (object or array depending
- *   on `isArray`). Once the data is returned from the server the existing reference is populated with the actual data.
- *
- *   The action methods on the class object or instance object can be invoked with the following
- *   parameters:
- *
- *   - HTTP GET "class" actions: `DjangoRESTResource.action([parameters], [success], [error])`
- *   - non-GET "class" actions: `DjangoRESTResource.action([parameters], postData, [success], [error])`
- *   - non-GET instance actions:  `instance.$action([parameters], [success], [error])`
- *
- *
- *   The DjangoRESTResource instances and collection have these additional properties:
- *
- *   - `$then`: the `then` method of a {@link http://docs.angularjs.org/api/ng.$q promise} derived from the underlying
- *     {@link http://docs.angularjs.org/api/ng.$http $http} call.
- *
- *     The success callback for the `$then` method will be resolved if the underlying `$http` requests
- *     succeeds.
- *
- *     The success callback is called with a single object which is the
- *     {@link http://docs.angularjs.org/api/ng.$http http response}
- *     object extended with a new property `resource`. This `resource` property is a reference to the
- *     result of the resource action — resource object or array of resources.
- *
- *     The error callback is called with the {@link http://docs.angularjs.org/api/ng.$http http response} object when
- *     an http error occurs.
- *
- *   - `$resolved`: true if the promise has been resolved (either with success or rejection);
- *     Knowing if the DjangoRESTResource has been resolved is useful in data-binding.
- */
+  // Helper functions and regex to lookup a dotted path on an object
+  // stopping at undefined/null.  The path must be composed of ASCII
+  // identifiers (just like $parse)
+  var MEMBER_NAME_REGEX = /^(\.[a-zA-Z_$@][0-9a-zA-Z_$@]*)+$/;
+
+  function isValidDottedPath(path) {
+    return (path !== null && path !== '' && path !== 'hasOwnProperty' &&
+        MEMBER_NAME_REGEX.test('.' + path));
+  }
+
+  function lookupDottedPath(obj, path) {
+    if (!isValidDottedPath(path)) {
+      throw $resourceMinErr('badmember', 'Dotted member path "@{0}" is invalid.', path);
+    }
+    var keys = path.split('.');
+    for (var i = 0, ii = keys.length; i < ii && angular.isDefined(obj); i++) {
+      var key = keys[i];
+      obj = (obj !== null) ? obj[key] : undefined;
+    }
+    return obj;
+  }
+
+  /**
+   * Create a shallow copy of an object and clear other fields from the destination
+   */
+  function shallowClearAndCopy(src, dst) {
+    dst = dst || {};
+
+    angular.forEach(dst, function(value, key) {
+      delete dst[key];
+    });
+
+    for (var key in src) {
+      if (src.hasOwnProperty(key) && !(key.charAt(0) === '$' && key.charAt(1) === '$')) {
+        dst[key] = src[key];
+      }
+    }
+
+    return dst;
+  }
+
+  /*
+   * Licence and
+   * Documentation
+  */
+
+  angular.module('DjRResources', ['ng']).
+    provider('$djrResource', function() {
+      var PROTOCOL_AND_DOMAIN_REGEX = /^https?:\/\/[^\/]*/;
+      var provider = this;
+
+      this.defaults = {
+        // Strip slashes by default
+        stripTrailingSlashes: true,
+        // Default actions configuration
+        actions: {
+          'get': {method: 'GET'},
+          'save': {method: 'POST'},
+          'query': {method: 'GET', isArray: true},
+          'remove': {method: 'DELETE'},
+          'delete': {method: 'DELETE'}
+        }
+      };
+
+      this.$get = ['$http', '$log', '$q', '$timeout', function($http, $log, $q, $timeout) {
+        var noop = angular.noop,
+          forEach = angular.forEach,
+          extend = angular.extend,
+          copy = angular.copy,
+          isFunction = angular.isFunction;
+
+        /**
+         * We need our custom method because encodeURIComponent is too aggressive and doesn't follow
+         * http://www.ietf.org/rfc/rfc3986.txt with regards to the character set
+         * (pchar) allowed in path segments:
+         *    segment       = *pchar
+         *    pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+         *    pct-encoded   = "%" HEXDIG HEXDIG
+         *    unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+         *    sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+         *                     / "*" / "+" / "," / ";" / "="
+         */
+        function encodeUriSegment(val) {
+          return encodeUriQuery(val, true).
+            replace(/%26/gi, '&').
+            replace(/%3D/gi, '=').
+            replace(/%2B/gi, '+');
+        }
+
+        /**
+         * This method is intended for encoding *key* or *value* parts of query component. We need a
+         * custom method because encodeURIComponent is too aggressive and encodes stuff that doesn't
+         * have to be encoded per http://tools.ietf.org/html/rfc3986:
+         *    query       = *( pchar / "/" / "?" )
+         *    pchar         = unreserved / pct-encoded / sub-delims / ":" / "@"
+         *    unreserved    = ALPHA / DIGIT / "-" / "." / "_" / "~"
+         *    pct-encoded   = "%" HEXDIG HEXDIG
+         *    sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+         *                     / "*" / "+" / "," / ";" / "="
+         */
+        function encodeUriQuery(val, pctEncodeSpaces) {
+          return encodeURIComponent(val).
+            replace(/%40/gi, '@').
+            replace(/%3A/gi, ':').
+            replace(/%24/g, '$').
+            replace(/%2C/gi, ',').
+            replace(/%20/g, (pctEncodeSpaces ? '%20' : '+'));
+        }
+
+        function Route(template, defaults) {
+          this.template = template;
+          this.defaults = extend({}, provider.defaults, defaults);
+          this.urlParams = {};
+        }
+
+        Route.prototype = {
+          setUrlParams: function(config, params, actionUrl) {
+            var self = this,
+              url = actionUrl || self.template,
+              val,
+              encodedVal,
+              protocolAndDomain = '';
+
+            var urlParams = self.urlParams = {};
+            forEach(url.split(/\W/), function(param) {
+              if (param === 'hasOwnProperty') {
+                throw $resourceMinErr('badname', "hasOwnProperty is not a valid parameter name.");
+              }
+              if (!(new RegExp("^\\d+$").test(param)) && param &&
+                (new RegExp("(^|[^\\\\]):" + param + "(\\W|$)").test(url))) {
+                urlParams[param] = {
+                  isQueryParamValue: (new RegExp("\\?.*=:" + param + "(?:\\W|$)")).test(url)
+                };
+              }
+            });
+            url = url.replace(/\\:/g, ':');
+            url = url.replace(PROTOCOL_AND_DOMAIN_REGEX, function(match) {
+              protocolAndDomain = match;
+              return '';
+            });
+
+            params = params || {};
+            forEach(self.urlParams, function(paramInfo, urlParam) {
+              val = params.hasOwnProperty(urlParam) ? params[urlParam] : self.defaults[urlParam];
+              if (angular.isDefined(val) && val !== null) {
+                if (paramInfo.isQueryParamValue) {
+                  encodedVal = encodeUriQuery(val, true);
+                } else {
+                  encodedVal = encodeUriSegment(val);
+                }
+                url = url.replace(new RegExp(":" + urlParam + "(\\W|$)", "g"), function(match, p1) {
+                  return encodedVal + p1;
+                });
+              } else {
+                url = url.replace(new RegExp("(\/?):" + urlParam + "(\\W|$)", "g"), function(match,
+                    leadingSlashes, tail) {
+                  if (tail.charAt(0) == '/') {
+                    return tail;
+                  } else {
+                    return leadingSlashes + tail;
+                  }
+                });
+              }
+            });
+
+            // strip trailing slashes and set the url (unless this behavior is specifically disabled)
+            if (self.defaults.stripTrailingSlashes) {
+              url = url.replace(/\/+$/, '') || '/';
+            }
+
+            // then replace collapse `/.` if found in the last URL path segment before the query
+            // E.g. `http://url.com/id./format?q=x` becomes `http://url.com/id.format?q=x`
+            url = url.replace(/\/\.(?=\w+($|\?))/, '.');
+            // replace escaped `/\.` with `/.`
+            config.url = protocolAndDomain + url.replace(/\/\\\./, '/.');
+
+
+            // set params - delegate param encoding to $http
+            forEach(params, function(value, key) {
+              if (!self.urlParams[key]) {
+                config.params = config.params || {};
+                config.params[key] = value;
+              }
+            });
+          }
+        };
+
+        function djrResourceFactory(){
+          var route = new Route(url, options);
+
+          actions = extend({}, provider.defaults.actions, actions);
+
+          function extractParams(data, actionParams) {
+            var ids = {};
+            actionParams = extend({}, paramDefaults, actionParams);
+            forEach(actionParams, function(value, key) {
+              if (isFunction(value)) { value = value(); }
+              ids[key] = value && value.charAt && value.charAt(0) == '@' ?
+                lookupDottedPath(data, value.substr(1)) : value;
+            });
+            return ids;
+          }
+
+          function defaultResponseInterceptor(response) {
+            return response.resource;
+          }
+
+          function DjrResource(value) {
+            shallowClearAndCopy(value || {}, this);
+          }
+
+          DjrResource.prototype.toJSON = function() {
+            var data = extend({}, this);
+            delete data.$promise;
+            delete data.$resolved;
+            return data;
+          };
+
+
+        }
+
+
+
+      }]; //END of this.$get
+
+    }); // END of PROVIDER
+
+
+})(window, window.angular);
+
 angular.module('djrResources', ['ng']).
   factory('djResource', ['$http', '$parse', function($http, $parse) {
     var DEFAULT_ACTIONS = {
